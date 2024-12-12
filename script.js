540e513a05617a3fd612e222a793f73538fb18d4
dhananjay8318 <dhananjay.23b0131059@abes.ac.in>
2024-12-12 15:39:27 +0530

    commitmyfiles

---
+++ script.js
@@ -0,0 +1,131 @@
+// function sayHello(){alert("Hellow World")}
+// document.write('this is first java script page');
+// document.write('<h1>it is a my forst program in java </h1>');
+// var pi=3.14;
+// var person="are you God";
+// var answer='yes i am';
+// document.write(pi+"<br>");
+// document.write(person+"<br>");
+// document.write(answer+"<br>");
+
+
+// var num=10;
+// console.log(num);
+// console.log(typeof(num));
+// num=false;
+// console.log(num);
+// console.log(typeof(num));
+// num="sanjeev";
+// console.log(num);
+// console.log(typeof(num));
+// num=null;
+// console.log(num);
+// console.log(typeof(num));
+// num=undefined;
+// console.log(num);
+// console.log(typeof(num));
+
+// var person=
+// {firstName:"John",
+// lastName: "Deo",
+// id:5566,
+// getinfo:function() {return this.firstName+""+this.lastName;
+// }
+// };
+// document.getElementById("demo").innerHTML=person.getinfo();
+// var person={
+//     name:'ramesh',
+//     age:30,
+//     isMarried:true,
+//     address:{
+//         street:'vinay nagar',
+//         flatno:201
+//     }
+// };
+// console.log(person);
+// var person={
+//     name:'Dhananjay singh ',
+//     age:21,
+//     isMarried:true,
+//     child:"jatin",
+//     getInfo:function(){
+
+//     },
+//     adress:{
+//         street:"varansi",
+//         flatNo:201
+//     }
+// };
+
+// function myFunction ()
+// {
+//     var carName="volvo";
+//     document.getElementById("this is a demo 1").innerHTML=typeof carName+""+carName;
+
+// }
+// myFunction();
+// document.getElementById("this is a demo 2").innerHTML=typeof carName;
+// var locates={
+//     europe:function(){
+//         var myFriend="Dhananjay";
+//         var france=function(){
+//             var paris=function(){
+//                 console.log(myFriend);
+//             };
+//             paris();
+//         };
+//         france();
+//     }
+// };
+// locates.europe();
+// function show(){
+//     var a="hii hello byee";
+//     document.write(a+"<br>");
+//     function disk(){
+//         var b="namaste jai shrree ram";
+//         document.write(b+"<br>");
+//     }
+//     disk();
+// }
+// show();
+// var myVar="global";
+// function check(){
+//     var myVar = "local";
+//     document.write(myVar);
+
+// }
+// var name='dhan';
+// function first(){
+//     var a ='hello';
+//     Second();
+//     var x = a + name ; 
+//     console.log(a) 
+// }
+// function Second(){
+//     var b = 'hi';
+//     third();
+//     var z=b+name;
+//     console.log(b)
+
+// }
+// function third(){
+//     var c = 'hye';
+//     third();
+//     var z=c+name;
+//     console.log(c)
+// }
+//     first();
+//conditional statement ;
+
+var age=21;
+if(age>18){
+    document.write("<b>You are eligible to cast the vote</b>");
+
+}
+
+
+
+
+
+
+
